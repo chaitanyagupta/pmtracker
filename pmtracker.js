217821fdec6e26d73d53a933dba538136333d0b0
Chaitanya Gupta <mail@chaitanyagupta.com>
2018-12-17 16:51:44 +0530

    Show controls

--- pmtracker.js
+++ pmtracker.js
@@ -1,6 +1,13 @@
 'use strict';
 
+let DEFAULT_LOCALE = 'en-IN';
+let DEFAULT_TIMEZONE = 'Asia/Kolkata';
+
 let apiKey = 'AIzaSyAxxmf1EGaVzxC_AZPkN0Mg_2vWRyJybtM';
+
+let getRandomInt = function (max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 let QS = {
     stringify: function (o) {
@@ -61,20 +68,28 @@ let resolveLocale = function () {
         }
     }
     // Return English (India) by default
-    return "en-IN";
+    return DEFAULT_LOCALE;
 };
 
-let fillInfoContainer = function (container, locationInfo) {
+let fillInfoContainer = function (container, locationInfo, place) {
     let locale = resolveLocale();
+    // image
+    if (place.photos && place.photos.length > 0) {
+        container.querySelector('.info-photo-container').hidden = false;
+        let img = container.querySelector('.info-photo');
+        let photo = place.photos[getRandomInt(place.photos.length)];
+        img.src = photo.getUrl({ maxWidth: 100, maxHeight: 100 });
+    }
     // date
     let date = new Date(locationInfo.date);
     let dateElement = container.querySelector('.info-date');
     dateElement.dateTime = locationInfo.date;
     dateElement.textContent = date.toLocaleString(locale, {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric"
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        timeZone: DEFAULT_TIMEZONE
     });
     // location name
     container.querySelector('.info-location-name').textContent = locationInfo.name;
@@ -87,36 +102,51 @@ let fillInfoContainer = function (container, locationInfo) {
     refAnchor.textContent = ref.source;
 };
 
-var initmap = function () {
-    if (!locationInfo.coords) {
-        console.warn('No coordinates');
-        return;
+let resolvePlace = function (service, locationName, callback) {
+    let request = {
+        query: locationName,
+        fields: ['photos', 'name', 'geometry']
     }
+    service.findPlaceFromQuery(request, function (results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            if (results.length > 0) {
+                let place = results[0];
+                callback(null, place);
+            } else {
+                console.warn('Got zero results back');
+            }
+        } else {
+            console.warn('find place from query status not ok', status);
+        }
+    });
+};
+
+var initmap = function () {
     if (typeof google === 'undefined' || !google.maps) {
         console.warn('No google');
         return;
     }
-    let map = new google.maps.Map(document.getElementById('map'), {
-        center: locationInfo.coords,
-        zoom: 8
+    let mapElement = document.getElementById('map');
+    let ps = new google.maps.places.PlacesService(mapElement);
+    resolvePlace(ps, locationInfo.name, function (error, place) {
+        let map = new google.maps.Map(mapElement, {
+            center: place.geometry.location,
+            zoom: 8
+        });
+        let marker = new google.maps.Marker({
+            position: place.geometry.location,
+            map: map
+        });
+        let infoContainer = document.getElementById('info-content');
+        fillInfoContainer(infoContainer, locationInfo, place);
+        infoContainer.hidden = false;
+        let infoWindow = new google.maps.InfoWindow({
+            content: infoContainer
+        });
+        infoWindow.open(map, marker);
+        let footerDiv = document.getElementById('footer');
+        footerDiv.hidden = false;
+        footerDiv.index = 1;
+        map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(footerDiv);
     });
-    let marker = new google.maps.Marker({ position: locationInfo.coords, map: map });
-    let infoContainer = document.getElementById('info-content');
-    fillInfoContainer(infoContainer, locationInfo);
-    infoContainer.hidden = false;
-    let infoWindow = new google.maps.InfoWindow({
-        content: infoContainer
-    });
-    infoWindow.open(map, marker);
 };
-
-if (!locationInfo.coords) {
-    geocode(locationInfo.name, function (error, response) {
-        if (error) {
-            console.warn('Couldn\'t get coords for location:', locationInfo.name);
-        } else {
-            locationInfo.coords = response.results[0].geometry.location;
-            initmap();
-        }
-    });
-}
