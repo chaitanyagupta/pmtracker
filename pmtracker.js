407710f538e2a04cc6cd28c50b5f4864ed6868cc
Chaitanya Gupta <mail@chaitanyagupta.com>
2018-12-18 15:25:35 +0530

    Everything seems to work now

--- pmtracker.js
+++ pmtracker.js
@@ -71,8 +71,23 @@ let resolveLocale = function () {
     return DEFAULT_LOCALE;
 };
 
+let ISODateString = function (date) {
+    let USDateString = date.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        timeZone: DEFAULT_TIMEZONE
+    });
+    let parts = USDateString.split('/');
+    return [parts[2], parts[0], parts[1]].join('-');
+};
+
 let fillInfoContainer = function (container, activity, place) {
     let locale = resolveLocale();
+    let today = ISODateString(new Date());
+    // headline
+    container.querySelector('.info-headline.today').hidden = (today !== activity.date);
+    container.querySelector('.info-headline.past').hidden = (today === activity.date);
     // image
     if (place.photos && place.photos.length > 0) {
         container.querySelector('.info-photo-container').hidden = false;
@@ -81,16 +96,20 @@ let fillInfoContainer = function (container, activity, place) {
         img.src = photo.getUrl({ maxWidth: 100, maxHeight: 100 });
     }
     // date
-    let date = new Date(activity.date);
     let dateElement = container.querySelector('.info-date');
     dateElement.dateTime = activity.date;
-    dateElement.textContent = date.toLocaleString(locale, {
-        weekday: 'long',
+    let dateOptions = {
         year: 'numeric',
         month: 'long',
         day: 'numeric',
         timeZone: DEFAULT_TIMEZONE
-    });
+    };
+    if (today === activity.date) {
+        dateElement.textContent = 'Today (' + (new Date(activity.date)).toLocaleString(locale, dateOptions) + ')';
+    } else {
+        dateOptions.weekday = 'long';
+        dateElement.textContent = (new Date(activity.date)).toLocaleString(locale, dateOptions);
+    }
     // location name
     container.querySelector('.info-location-name').textContent = activity.location;
     // activity
@@ -121,35 +140,128 @@ let resolvePlace = function (service, locationName, callback) {
     });
 };
 
+let bounds = function (start, end) {
+    return [start, end];
+};
+
+let startBounds = function (bounds) { return bounds[0]; };
+let endBounds = function (bounds) { return bounds[1]; };
+
+let equalBounds = function (b1, b2) {
+    return startBounds(b1) === startBounds(b2) && endBounds(b1) === endBounds(b2);
+};
+
+let parseDateBounds = function (query) {
+    query = query || QS.parse(window.location.search);
+    let now = new Date();
+    let today = ISODateString(now);
+    if (query.start) {
+        if (query.start.match(/^[0-9]+d/)) {
+            let days = parseInt(query.start.match(/^[0-9]+/));
+            let start = ISODateString(new Date(now - ((days - 1) * MSEC_PER_DAY)));
+            return bounds(start, today);
+        } else if (query.start === 'today') {
+            return bounds(today, today);
+        }
+    } else {
+        // Return today for unrecognized strings
+        return bounds(today, today);
+    }
+};
+
+let MSEC_PER_DAY = 86400000;
+
+let filterActivities = function (bounds) {
+    return allActivities.filter(function (activity) {
+        return activity.date >= startBounds(bounds) && activity.date <= endBounds(bounds);
+    });
+};
+
+let selectedActivities = function () {
+    let activities = filterActivities(parseDateBounds());
+    if (activities.length > 0) {
+        return activities;
+    } else {
+        return [allActivities[allActivities.length - 1]];
+    }
+};
+
+let closeInfoWindow = function (infoWindow) {
+    infoWindow.close();
+    infoWindow.content.hidden = true;
+    document.body.appendChild(infoWindow.content);
+};
+
+let displayInfo = function (map, activity, place, marker, current) {
+    if (current) { 
+        closeInfoWindow(current);
+    }
+    let infoContainer = document.getElementById('info-content');
+    fillInfoContainer(infoContainer, activity, place);
+    infoContainer.hidden = false;
+    let infoWindow = new google.maps.InfoWindow({
+        content: infoContainer
+    });
+    infoWindow.open(map, marker);
+    return infoWindow;
+};
+
+let displayActivities = function (context) {
+    let activities = selectedActivities();
+    let lastActivity = activities[activities.length - 1];
+    let today = ISODateString(new Date());
+    activities.forEach(function (activity) {
+        resolvePlace(context.ps, activity.location, function (error, place) {
+            let marker = new google.maps.Marker({
+                position: place.geometry.location,
+                map: context.map,
+                title: activity.location,
+                opacity: (activity.date === today ? 1.0 : 0.5)
+            });
+            marker.addListener('click', function () {
+                context.infoWindow = displayInfo(context.map, activity, place, marker, context.infoWindow);
+            });
+            if (activity === lastActivity) {
+                context.infoWindow = displayInfo(context.map, activity, place, marker, context.infoWindow);
+            }
+        });
+    });
+};
+
 var initmap = function () {
     if (typeof google === 'undefined' || !google.maps) {
         console.warn('No google');
         return;
     }
     let mapElement = document.getElementById('map');
-    let ps = new google.maps.places.PlacesService(mapElement);
-    resolvePlace(ps, activity.location, function (error, place) {
-        let map = new google.maps.Map(mapElement, {
-            center: place.geometry.location,
-            zoom: 8,
-            fullscreenControl: false
-        });
-        let marker = new google.maps.Marker({
-            position: place.geometry.location,
-            map: map
-        });
-        let infoContainer = document.getElementById('info-content');
-        fillInfoContainer(infoContainer, activity, place);
-        infoContainer.hidden = false;
-        let infoWindow = new google.maps.InfoWindow({
-            content: infoContainer
-        });
-        infoWindow.open(map, marker);
-        let tpControlsDiv = document.getElementById('time-period-controls');
-        tpControlsDiv.hidden = false;
-        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(tpControlsDiv);
-        let footerDiv = document.getElementById('footer');
-        footerDiv.hidden = false;
-        map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(footerDiv);
+    let map = new google.maps.Map(mapElement, {
+        center: { lat: 28.61, lng: 77.20 },
+        zoom: 5,
+        fullscreenControl: false
+    });
+    let ps = new google.maps.places.PlacesService(map);
+    let context = { map: map, ps: ps, infoWindow: null };
+    displayActivities(context);
+    let tpControlsDiv = document.getElementById('time-period-controls');
+    tpControlsDiv.hidden = false;
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(tpControlsDiv);
+    let footerDiv = document.getElementById('footer');
+    footerDiv.hidden = false;
+    map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(footerDiv);
+    map.addListener('click', function () {
+        if (context.infoWindow) {
+            closeInfoWindow(context.infoWindow);
+        }
     });
 };
+
+document.querySelectorAll('input[name=start]').forEach(function (input) {
+    let pageBounds = parseDateBounds();
+    let inputBounds = parseDateBounds({ start: input.value });
+    if (equalBounds(inputBounds, pageBounds)) {
+        input.checked = true;
+    }
+    input.addEventListener('change', function () {
+        input.form.submit();
+    });
+});
